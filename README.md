db5353491732b100dd2af274289cbae628d6fffc
xuxiaozhou <759811542@qq.com>
2021-09-05 20:32:59 +0800

    usage

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pd-cli
 脚手架
+
+## 安装脚手架
+```
+npm install xz-pd-cli -g
+yarn add xz-pd-cli -g
+```
+
+## 命令行
+```
+pd create <project name>
+```
--- aaa/build/dev.ts
+++
@@ -1,5 +0,0 @@
-import commonConfig from './base';
-import { merge } from 'webpack-merge';
-module.exports = merge(commonConfig, {
-  mode: 'development',
-});
